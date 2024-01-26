f99ffdd18e1c5233a947bf8021ae51e961c14a03
-3σ <yurippe@m3sigma.net>
2024-01-26 15:21:49 +0900

    comments

--- numpytorch/nn.py
+++ numpytorch/nn.py
@@ -5,10 +5,15 @@ from .functions import *
 
 
 class Parameter(Tensor):
+    """
+    To manage the tensors used as parameters in the model separately,
+    we created this class that inherits from Tensor.
+    """
     def __init__(self, x: Tensor) -> None:
         super().__init__(arr=x, requires_grad=True)
 
     def _init_weight(*args: int) -> Tensor:
+        # He Uniform Initialization
         u = (6 / args[0])**0.5
         return tensor(np.random.uniform(-u, u, size=args))
 
@@ -16,6 +21,11 @@ class Parameter(Tensor):
         return Parameter(Parameter._init_weight(*args))
 
 class Module:
+    """
+    A class for conveniently managing each layer, module, or model of a DNN.
+    If you want to create a new layer, you can create a subclass that inherits
+    from Module and just implement the forward method.
+    """
     def _forward_unimplemented(*args, **kwargs) -> None:
         raise Exception("forward not implemented")
     forward: Callable[..., Any] = _forward_unimplemented
@@ -24,6 +34,13 @@ class Module:
         return self.forward(*args, **kwargs)
 
     def parameters(self) -> List[Parameter]:
+        """
+        In order to optimize a model during training, the values of the parameters inside
+        the model must be constantly updated. This is done through the optimizer in optim.py,
+        which requires a list of all the parameters (Parameter) a model (or module) has.
+        If a Module contains other Modules as attributes, it will also return the parameters
+        of those Modules.
+        """
         params: List[Parameter] = []
         for v in self.__dict__.values():
             if isinstance(v, Module):
@@ -42,6 +59,12 @@ class Linear(Module):
         return x @ self.w + self.b
 
 class Sequential(Module):
+    """
+    It is often the case that multiple layers need to be applied in succession, each taking
+    a single tensor as input and returning a single tensor (e.g. CNN). It's tedious to assign
+    each layer an attribute for this process and apply each one directly in the forward, so we
+    can wrap it in a simple Module.
+    """
     def __init__(self, *args) -> None:
         for i, module in enumerate(args):
             setattr(self, str(i), module)
